4e754b33ac03d1bfd78a7bb5a6eea91e55d97710
Mo'men Soliman <moumensoliman@gmail.com>
2018-01-26 22:28:36 +0200

    Long-Term Caching

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,20 @@ const path = require('path');
 const glob = require('glob');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const PurifyCSSPlugin = require('purifycss-webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const inProduction = (process.env.NODE_ENV === 'production');
 
 module.exports = {
     entry: {
-        app: [
+        main: [
             './src/main.js',
             './src/main.scss'
-        ]
+        ],
+        vendor: ['jquery']
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
+        filename: '[name].[chunkhash].js'
     },
     watchOptions: {
         poll: true
@@ -53,13 +55,18 @@ module.exports = {
         ]
     },
     plugins: [
-        new ExtractTextPlugin('[name].css'),
+        new ExtractTextPlugin('[name].[contenthash].css'),
         new webpack.LoaderOptionsPlugin({
           minimize: inProduction
         }),
         new PurifyCSSPlugin({
             paths: glob.sync(path.join(__dirname, 'index.html')),
             minimize: inProduction
+        }),
+        new CleanWebpackPlugin(['dist', 'build'], {
+            root: __dirname,
+            verbose: true,
+            dry: false
         })
     ]
 };
