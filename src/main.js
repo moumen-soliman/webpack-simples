d3d38bdc699c91941e16d3436e06f26d1c9503ab
Mo'men Soliman <moumensoliman@gmail.com>
2018-01-26 21:28:12 +0200

    Dedicated Configuration File

---
+++ src/main.js
@@ -0,0 +1,1 @@
+alert('update this again and again and again');
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    watchOptions: {
+        poll: true
+    }
+};
