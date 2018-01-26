6f780d54c80cfcb046c28bfa7ab820a6884fd27e
Mo'men Soliman <moumensoliman@gmail.com>
2018-01-26 21:51:28 +0200

    Sass Compilation

--- src/main.js
+++ src/main.js
@@ -1,9 +1,1 @@
-class Form {
-    constructor() {
-        let numbers = [5, 10, 15].map(number => number * 2);
-
-        console.log(numbers);
-    }
-}
-
-new Form();
+require('./main.scss')
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,14 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.s[ac]ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
                 test: /\.css$/,
                 use: [
                     'style-loader',
